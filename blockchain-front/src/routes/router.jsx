6ef393176b81ca765125e433aa599fa5a9c8e86b
LeticiaSolis <20223tn112@utez.edu.mx>
2025-10-29 12:54:24 -0600

    ADD: Implementacion de tailwind

--- blockchain-front/src/routes/router.jsx
+++ blockchain-front/src/routes/router.jsx
@@ -1,12 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import { Navigate } from "react-router";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/navbar";
 
-const AppRouter = ()=>{
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route></Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex flex-col items-center justify-center h-screen bg-light">
+              <h1 className="text-4xl font-bold text-primary mb-4">
+                ¡Bienvenida a Token CO₂ Caña! 🌿
+              </h1>
+              <p className="text-accent text-lg">
+                Tailwind y React Router están funcionando correctamente.
+              </p>
+            </div>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
